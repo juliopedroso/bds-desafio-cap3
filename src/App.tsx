71224daf1e5a12a580e77911ecb555a295893509
Julio Cesar Pedroso <juliocp@weg.net>
2024-01-16 16:41:21 -0300

    Home page created

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import Home from 'pages/Home';
 import './App.css';
 
 function App() {
   return (
-    <h1>Hello MovieFlix</h1>
-    );
+    <Home />
+  );
 }
 
 export default App;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,7 @@
+const Home = () => {
+    return (
+        <h1>MovieFlix - Homepage</h1>
+    );
+};
+
+export default Home;
