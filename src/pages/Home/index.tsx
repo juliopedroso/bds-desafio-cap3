4724085cf70284c83b5772f431a015f32705f4ba
Julio Cesar Pedroso <juliocp@weg.net>
2024-01-18 08:10:06 -0300

    Add home page content

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,18 @@
+import { ReactComponent as MainImage } from 'assets/images/main-image.svg'
 import Login from "./Login";
 
 const Home = () => {
     return (
-        <Login/>
+        <div className="home-container">
+            <div className="home-banner-container">
+                <h1>Avalie Filmes</h1>
+                <p>Diga o que você achou do seu filme favorito</p>
+                <MainImage />
+            </div>
+            <div className="home-form-container">
+                <Login />
+            </div>
+        </div>
     );
 };
 
