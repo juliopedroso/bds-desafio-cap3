0fec54db35f4daf2a01f10853ef4b47e1b079868
Julio Cesar Pedroso <juliocp@weg.net>
2024-01-30 07:21:02 -0300

    Create MovieDetails

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,6 +3,7 @@ import Home from 'pages/Home';
 import Navbar from 'components/Navbar';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import MovieCatalog from 'pages/Private/MovieCatalog';
+import MovieDetails from 'pages/Private/MovieDetails';
 
 const Routes = () => (
     <BrowserRouter>
@@ -11,9 +12,11 @@ const Routes = () => (
             <Route path="/" exact>
                 <Home />
             </Route>
-
-            <Route path="/movies" >
+            <Route path="/movies" exact>
                 <MovieCatalog />
+            </Route>
+            <Route path="/movies/:movieid" >
+                <MovieDetails />
             </Route>
         </Switch>
     </BrowserRouter>
