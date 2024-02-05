b154444119e18943be8fc990536711e243edead6
Julio Cesar Pedroso <juliocp@weg.net>
2024-02-05 07:48:31 -0300

    create Logout button

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,13 +1,21 @@
 import './styles.css';
 
 const Navbar = () => {
+    const handleLogoutClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
+        event.preventDefault();
+    }
+
     return (
         <nav className="navbar navbar-expand-md navbar-dark bg-warning main-nav">
             <div className="container-fluid">
                 <div className="nav-logo-text">
                     <h4>MovieFlix</h4>
                 </div>
-                
+                <div className='btn nav-logout'>
+                    <a href='#logout' onClick={handleLogoutClick}>
+                        SAIR
+                    </a>
+                </div>
             </div>
         </nav>
     );
